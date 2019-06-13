fdbed948be2cf6dbc813dd25da7ed6eff893d801
Christian Bjork <cc.bjoerk@gmail.com>
2019-06-14 00:12:42 +0200

    Testing/finish inputs

--- blackjack.py
+++ blackjack.py
@@ -1,6 +1,5 @@
 # BlackJack game
-# Github test this is a change hey what is this is change
-import os
+
 import random
 import time
 from chips import Chips
@@ -13,28 +12,30 @@ def take_bet(chips):
 
     while True:
         try:
+            #Place the bet
             print("Your chips total is: ", chips.total)
             chips.bet = int(input("How many chips would you like to bet?: "))  
         except ValueError:
-            print("You have to bet an integer!")
+            print("You have to bet a number!")
         else:
             if chips.bet > chips.total:
-                print("You bet can't be higher than your chips total: ", chips.total)
+                print("You bet can't be higher than your total amount of chips: ", chips.total)
             elif chips.bet < 0:
                 print("You can't bet a negative value!")
             else:
                 break     
 
-playing = True
 
 
-while playing == True:
+
+while True:
     #Dealer cards
     dealer_cards = []
 
     #Player cards
     player_cards = []
 
+    #Choose to be a player or dealer
     dealer_or_player = str(input("Do you want do deal or play? "))
 
 
@@ -58,6 +59,7 @@ while playing == True:
             #Sum of the Player cards
 
         while sum(player_cards) < 21:
+            #Deal the cards or 
             action_taken = str(input("Do you want to stay or hit? "))
             if action_taken == "hit":
                 player_cards.append(random.randint(1, 11))
@@ -95,6 +97,7 @@ while playing == True:
                             print("Dealer Wins")   
                             Chips.lose_bet(player_chips)
                             dealer_cards.append(1)
+                            print(dealer_cards)
                         elif sum(dealer_cards) > 21:
                             print("The dealer has busted! " + str(sum(dealer_cards)) + " with ", dealer_cards)
                             print("You have a total of " + str(sum(player_cards)) + " with ", player_cards)
@@ -102,8 +105,6 @@ while playing == True:
                             Chips.win_bet(player_chips)
                                 
                     break
-
-                    
 
 
         if sum(player_cards) > 21:
@@ -114,42 +115,50 @@ while playing == True:
             Chips.win_bet(player_chips)  
 
 
-        
-
-
-    if dealer_or_player == "deal":
+    elif dealer_or_player == "deal":
         while len(dealer_cards) != 2:
             dealer_cards.append(random.randint(1, 11))
             if len(dealer_cards) == 2:
-                print("You have X & ", dealer_cards[1])
+                print("You have:   [ X &", dealer_cards[1],"]")
 
         # player cards
         while len(player_cards) != 2:
             player_cards.append(random.randint(1, 11))
             if len(player_cards) == 2:
-                print("Player has ", player_cards)
+                print("Player has: [", player_cards[0], "&", player_cards[1], "]")
 
         # sum of player cards
         while sum(player_cards) < 21:
-            if sum(player_cards) < 14:
+            time.sleep(2)
+            if sum(player_cards) <= 14:
                 player_cards.append(random.randint(1,11))
                 print('Player: "Hit me!"')
                 print("Player now has a total of " + str(sum(player_cards)) + " from these cards ", player_cards)
-
+            
                 if sum(player_cards) == 21:
                     print("Player has BLACKJACK! Player Wins!")
                 elif sum(player_cards) > 21:
                     print("Player busted! You win")
+
+            elif (sum(player_cards) > 14 and sum(player_cards) == sum(dealer_cards)):
+                print('Player: "I stay!"')
+                time.sleep(2)
+                print("You have a total of " + str(sum(dealer_cards)) + " with ", dealer_cards)
+                print("Player has a total of " + str(sum(player_cards)) + " with ", player_cards)
+                print("You win!")
+                break  
             else:  
-            
+                print('Player: "I stay!"')
+                time.sleep(2)
+                print("You have:   [", dealer_cards[0], "&", dealer_cards[1], "] with a total of " + str(sum(dealer_cards)))
                 if sum(dealer_cards) > sum(player_cards):
                     print("You have a total of " + str(sum(dealer_cards)) + " with ", dealer_cards)
                     print("The player has a total of " + str(sum(player_cards)) + " with ", player_cards)
                     print("You win!")
                     break    
                 else:
-                    while sum(dealer_cards) < 17:
-                        deal_a_card = str(input('The players cards are higher than yours. Press "ENTER" to turn the next card '))
+                    while sum(dealer_cards) < 21:
+                        deal_a_card = str(input('The players cards are higher than yours, with a total of {}. Press "ENTER" to turn the next card '.format(sum(player_cards))))
                         dealer_cards.append(random.randint(1, 11))
 
                         if(sum(dealer_cards) > sum(player_cards) and sum(dealer_cards) < 21):
@@ -169,8 +178,10 @@ while playing == True:
                         elif sum(dealer_cards) > 21:
                             print("You have Busted! " + str(sum(dealer_cards)) + " with ", dealer_cards)
                             print("Player has a total of " + str(sum(player_cards)) + " with ", player_cards)
-                            print("Player wins!")
-                        print(str(sum(dealer_cards)))    
+                            print("Player wins!") 
+                        elif sum(player_cards) > sum(dealer_cards):
+                            print("You have a total of " + str(sum(dealer_cards)) + " with ", dealer_cards)
+                            print("Player has a total of " + str(sum(player_cards)) + " with ", player_cards)   
                     if sum(player_cards) > sum(dealer_cards):
                         print("You have a total of " + str(sum(dealer_cards)) + " with ", dealer_cards)
                         print("Player has a total of " + str(sum(player_cards)) + " with ", player_cards)
@@ -178,18 +189,28 @@ while playing == True:
 
                     break
 
-    if player_chips.total == 0:
-        playing = False
-        print("You are out of Chips! Thank you for playing!")
     else:
-        print("Thanks for playing! Your winnings stand at: ", player_chips.total, "Chips!")
+        print("I'm sorry, that is not possible, you can only deal or play.\n")
+        continue    
+
+    if dealer_or_player == "play":
+        if player_chips.total == 0:
+            print("You are out of Chips! Thank you for playing!")
+            break
+        else:
+            print("Your winnings stand at: ", player_chips.total, "Chips!")
+            new_game = str(input('Would you like to play another game? Press "y" or "n"'))
+
+            if new_game != "y":
+                print("Thank you for playing!")
+                break
+    
+    elif dealer_or_player == "deal":
         new_game = str(input('Would you like to play another game? Press "y" or "n"'))
 
-        if new_game[0] == "y":
-            playing = True
-        else:    
-            playing = False
+        if new_game != "y":
             print("Thank you for playing!")
-    
-    
+            break
+
+
    
